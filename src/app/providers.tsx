7ebb652af6f456b9f23252280468509645540f6e
Taki Wong <taki.wong@vts.com>
2025-10-05 14:42:10 -0400

    Fix: Remove AuthContext dependency to enable direct access to Activate page

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,16 +3,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { AppContextProvider } from "./context/AppContext";
-import { AuthContextProvider } from "./context/AuthContext";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <AppContextProvider>{children}</AppContextProvider>
-      </AuthContextProvider>
+      <AppContextProvider>{children}</AppContextProvider>
     </QueryClientProvider>
   );
 }
